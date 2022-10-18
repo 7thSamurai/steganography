821d2b8021910ed9c1f83d9042ecb37e114bce98
Zach Collins <programmer255@pm.me>
2022-10-18 17:11:17 -0400

    Added data size to string helper function

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
+#include <string>
+#include <sstream>
+
+inline std::string data_size(std::size_t size) {
+    std::stringstream ss;
+
+    if (size >= 1024*1024)
+        ss << std::fixed << std::setprecision(2) << size / float(1024*1024) << " MiB";
+    else if (size >= 1024)
+        ss << std::fixed << std::setprecision(2) << size / float(1024) << " KiB";
+    else
+        ss << size << " B";
+
+    return ss.str();
+}
+
 // Rotate bits left shift places
 template <typename T> T rotl(const T &t, std::size_t shift) {
     constexpr std::size_t bits = sizeof(T) * 8;
